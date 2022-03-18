d9cfba7dc9440b32e7f360c5d149c9bf288844de
Eliphaz <eliahlander3@gmail.com>
2022-03-18 16:21:38 -0600

    added await in tests

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -7,7 +7,6 @@ const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
     driver.get('http://localhost:3000/')
-    driver.sleep(1000)
 })
 
 afterAll(async () => {
@@ -17,25 +16,23 @@ afterAll(async () => {
 test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
-    driver.sleep(5000)
     expect(displayed).toBe(true)
     
 })
 
 test('clicking draw button displays bot choices', async ()=>{
-    driver.findElement(By.id('draw')).click()
+    await driver.findElement(By.id('draw')).click()
     const choices = await driver.findElement(By.id('choices'))
-    driver.sleep(5000)
     expect(await choices.isDisplayed()).toBe(true)
     
 })
 
 test('clicking bot displays bot duo', async ()=>{
-    driver.findElement(By.id('draw')).click()
-    driver.sleep(1000)
-    driver.findElement(By.className('bot-btn'[1])).click()
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(5000)
+    await driver.findElement(By.className('bot-btn')).click()
+    await driver.sleep(5000)
     const playerDuo = await driver.findElement(By.id('player-duo'))
-    driver.sleep(5000)
     expect(await playerDuo.isDisplayed()).toBe(true)
     
 })
